80566fe1abecbb777d6da6e79452f648e5b9b0dd
tomandrews_h <tomandrews_h@dyn3181-41.wlan.ic.ac.uk>
2020-11-12 15:54:38 +0000

    using interrupts

--- DAC_Interrupt.s
+++ DAC_Interrupt.s
@@ -11,14 +11,14 @@ DAC_Int_Hi:
 	call	read_col
 	movlw	0xff
 	cpfseq	combined_input
+	bra	DAC_Int_Hi
 	bcf	TMR0IF		; clear interrupt flag
-	bsf	T0CON, 7
 	retfie	f		; fast return from interrupt
 
 DAC_Setup:
 	clrf	TRISJ, A	; Set PORTD as all outputs
 	clrf	LATJ, A		; Clear PORTD outputs
-	movlw	10000111B	; Set timer0 to 16-bit, Fosc/4/256
+	movlw	10000011B	; Set timer0 to 16-bit, Fosc/4/16
 	movwf	T0CON, A	; = 62.5KHz clock rate, approx 1sec rollover
 	bsf	TMR0IE		; Enable timer0 interrupt
 	bsf	GIE		; Enable all interrupts
